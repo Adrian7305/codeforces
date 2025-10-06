84d91271f81ddb8eb9cb3f37dc232a174b330803
Adrian7305 <adrianronan7305@gmail.com>
2025-10-06 17:31:41 +0530

    day 7

---
+++ soldierBanana.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class soldierBanana{
+    Scanner sc = new Scanner(System.in)
+    int k = sc.nextInt();
+    int n = sc.nextInt();
+    int w = sc.nextInt();
+    
+}
