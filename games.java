334136661be0812cb658bc5cefd9c65db2d9d1a2
Adrian7305 <adrianronan7305@gmail.com>
2025-10-26 22:58:43 +0530

    day i really dk

---
+++ games.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class games {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] uniforms = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            uniforms[i][0] = sc.nextInt();
+            uniforms[i][1] = sc.nextInt();
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && uniforms[i][0] == uniforms[j][1]) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+        sc.close();
+    }
+}
