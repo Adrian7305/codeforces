61ae4635a4e4bae9e862cecc289f225ab06dbb6c
Adrian7305 <adrianronan7305@gmail.com>
2025-12-15 20:39:17 +0530

    array colouring

---
+++ arrayColouring.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class arrayColouring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int odd = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (sc.nextInt() % 2 != 0) {
+                    odd++;
+                }
+            }
+
+            if (odd % 2 == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+}
